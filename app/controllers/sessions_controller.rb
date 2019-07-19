3ce4882ae4bee668a8be95671bce243521e359f1
Mohammed Noori <kmanoorib@gmail.com>
2019-07-19 17:38:56 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,10 @@
 class SessionsController < ApplicationController
   
   def create
-	  @user = User.find_or_create_by(uid: auth['uid') do |u|
-		  u.name = auth['info']['name']
-		  u.uid = auth['info']['uid']
-	  end
-	
-	  session[:user_id] = @user.id
-	
-	  render 'welcome/home'
+    user = User.find_or_create_by(:uid => auth['uid']) do |user|
+      user.name = auth['info']['name']
+    end
+    session[:user_id] = user.try(:id)
   end
   
   private
